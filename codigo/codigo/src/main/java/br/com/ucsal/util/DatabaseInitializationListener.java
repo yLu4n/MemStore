70d4b3f08be2e0ff216e6c4fc149fcf12802b23c
Luan Victor <107703846+yLu4n@users.noreply.github.com>
2024-11-14 20:27:47 -0300

    Initial Commit

---
+++ codigo/codigo/src/main/java/br/com/ucsal/util/DatabaseInitializationListener.java
@@ -0,0 +1,20 @@
+package br.com.ucsal.util;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class DatabaseInitializationListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Iniciando o banco de dados.");
+        DatabaseUtil.iniciarBanco();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Encerrando Aplicação");
+    }
+}
